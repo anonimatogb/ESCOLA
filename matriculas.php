c756e6b84848f317709928b8505a8f1b3d1fd3b8
anonimatogb <gabrielcavalcante22@outlook.com>
2026-04-10 10:29:33 -0300

    matriculas

--- matriculas.php
+++ matriculas.php
@@ -2,7 +2,14 @@
 
 require_once 'config.php';
 
-$sql= "SELECT * FROM matriculas";
+$sql= "SELECT 
+alunos.nome AS aluno,
+cursos.nome AS curso,
+matriculas.id,
+matriculas.data_matricula
+FROM matriculas
+INNER JOIN alunos ON matriculas.aluno_id = alunos.id
+INNER JOIN cursos ON matriculas.cursos_id = cursos.id";
 $stmt=$pdo->query($sql);
 
 echo "<h1>Matriculas</h1>";
@@ -11,8 +18,8 @@ echo "<tr><th>ID</th><th>ID Aluno</th><th>ID Curso</th><th>Data Matricula</th></
 foreach ($stmt as $row) {
     echo "<tr>";
     echo "<td>" . ($row['id']) . "</td>";
-    echo "<td>" . ($row['aluno_id']) . "</td>";
-    echo "<td>" . ($row['cursos_id']) . "</td>";
+    echo "<td>" . ($row['aluno']) . "</td>";
+    echo "<td>" . ($row['curso']) . "</td>";
     echo "<td>" . ($row['data_matricula']) . "</td>";
     echo "</tr>";
 }
